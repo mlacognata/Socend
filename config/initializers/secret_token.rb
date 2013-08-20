e46f26e87b6cdc2d135eeb84f0ece3ee9bb08d83
Michael Lacognata <mlacognata@gmail.com>
2013-08-20 16:31:44 -0600

    Initial Commit of Socend

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Socend::Application.config.secret_token = 'f864ce6e2478fc521adee469320213270a2666a3cebed46158d7ce569ee524bb9d4a1ce77bc7e2f2aa74217aca0523663ee8b8e25cecd9e405abf89dc3eb7a55'
