31fb185a71c3d52b7dbcf74415d4a1101940dbe1
Michael Lacognata <mlacognata@gmail.com>
2013-08-23 11:18:31 -0600

    Messing with hero unit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Socend::Application.routes.draw do
+  get 'about' => 'pages#about'
+
+   root :to => 'pages#home'
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
